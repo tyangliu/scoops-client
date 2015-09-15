e98a00c647faec05f1b10ec5b797066bbfe741fc
tyangliu <tyangliu@gmail.com>
2015-09-14 17:57:28 -0700

    Check in article related components

---
+++ src/components/ArticleTile.jsx
@@ -0,0 +1,59 @@
+'use strict';
+
+import React, { Component } from 'react';
+import Radium from 'radium';
+import styler from 'react-styling';
+import { Link } from 'react-router';
+import moment from 'moment';
+
+@Radium
+export default class ArticleTile extends Component {
+
+  static defaultProps = {
+    title: 'Article Title',
+    createdOn: '',
+    summary: ''
+  };
+
+  render() {
+    let title = this.props.title,
+        createdOn = moment(this.props.createdOn).format('ddd, MMMM M, YYYY'),
+        summary = this.props.summary;
+
+    return (
+      <div style={styles.articleTile}>
+        <div style={styles.thumbnail}></div>
+        <div style={styles.content}>
+          <Link to=''><h3>{title}</h3></Link>
+          <p style={styles.date}>{createdOn}</p>
+          <p style={styles.summary}>{summary}</p>
+        </div>
+      </div>
+    );
+  }
+
+}
+
+const styles = styler`
+  articleTile
+    width: 100%
+    height: 100%
+
+  thumbnail
+    width: 100%
+    height: 280px
+    background: #4b74b5
+
+  content
+    padding: 24px
+
+  date
+    font-size: 14px
+    text-transform: uppercase
+    letter-spacing: 1px
+    opacity: 0.85
+
+  summary
+    margin: 24px 0 36px
+
+`;
---
+++ src/components/HArticleList.jsx
@@ -0,0 +1,57 @@
+'use strict';
+
+import React, { Component } from 'react';
+import Radium from 'radium';
+import styler from 'react-styling';
+import _ from 'lodash';
+import ArticleTile from './ArticleTile.jsx';
+
+@Radium
+export default class HArticleList extends Component {
+
+  static defaultProps = {
+    articles: []
+  };
+
+  render() {
+    let articleTiles = _.chain(this.props.articles)
+      .map((article,index) =>
+        <div style={styles.articleTileContainer} key={'articleTile' + index}>
+          <ArticleTile title={article.title}
+                       createdOn={article.createdOn}
+                       summary={article.summary} />
+        </div>
+      )
+      .chunk(3)
+      .map(articles =>
+        <div>
+          {articles} <div style={styles.clearfix}></div>
+        </div>)
+      .value();
+
+    return (
+      <section style={styles.articleList}>
+        {articleTiles}
+      </section>
+    );
+  }
+
+}
+
+const styles = styler`
+  articleList
+    margin: 0 12px
+
+  articleTileContainer
+    width: 33.33%
+    float: left
+    padding: 0 12px
+
+    @media (max-width: 960px)
+      width: 100%
+      display: block
+
+  clearfix
+    clear: both
+    display: table
+`;
