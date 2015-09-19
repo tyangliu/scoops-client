23785715c41c4b2a624b72f5dcf174a3e368cd8f
tyangliu <tyangliu@gmail.com>
2015-09-19 06:18:39 -0700

    Remove trailing comma in webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -17,7 +17,7 @@ module.exports = {
     new webpack.DefinePlugin({'process.env': {NODE_ENV: '"production"'}}),
     new webpack.optimize.OccurenceOrderPlugin(),
     new webpack.optimize.DedupePlugin(),
-    // new webpack.optimize.UglifyJsPlugin({minimize: true}),
+    // new webpack.optimize.UglifyJsPlugin({minimize: true})
   ],
   module: {
     loaders: [
